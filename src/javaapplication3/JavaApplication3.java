762e9f70d3e9ae182da6f0bbabe701a943813a66
B1 <B1@B130>
2017-11-01 13:08:08 +0800

    My first commit

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -17,6 +17,8 @@ public class JavaApplication3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hi");
+        System.out.println("yo");
+        
     }
     
 }
